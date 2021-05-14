8a70f9c6f9e8da50e5dd843afbb4f16c6dfae30d
Giridhar <giridhar.balaz@gmail.com>
2021-05-14 17:14:20 +0530

    Changes

--- Worker.cs
+++ Worker.cs
@@ -18,10 +18,38 @@ namespace CoWinSlotNotifier
 
         private IConfiguration config;
 
+        private int age_limit;
+
+        private string userAgent;
+
+        private string apiAddress;
+
+        private string FeeType;
+
+        private string districtName;
+
         public Worker(ILogger<Worker> logger, IConfiguration config)
         {
             _logger = logger;
             this.config = config;
+
+            age_limit = Convert.ToInt32(config["Age_Limit"]);
+            userAgent = config["User_Agent"];
+            apiAddress = config["Api_Url"];
+            FeeType = config["Fee_Type"];
+            districtName = config["District"];
+
+            _logger.LogInformation($"Finding slots for {districtName}...");
+
+            int districtId;
+            if (!Constants.DistrictMapping.TryGetValue(districtName, out districtId))
+            {
+                _logger.LogError("District not found, please refer valid values of district from CSV file");
+                return;
+            }
+
+            string dateString = DateTime.Now.ToString("dd-MM-yyyy");
+            apiAddress = string.Format(apiAddress, districtId, dateString);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -29,37 +57,15 @@ namespace CoWinSlotNotifier
             // Put the district number and date
             Dictionary<int, Center> availableCenters = new Dictionary<int, Center>();
 
-            int districtId;
-            while (true)
-            {
-                Console.WriteLine("Please enter the district: ");
-                string districtName = Console.ReadLine();
-                
-                if (Constants.DistrictMapping.TryGetValue(districtName, out districtId))
-                {
-                    break;
-                }
-                Console.WriteLine("District not found, please refer valid values of district");
-            }         
-
-            int age_limit = Convert.ToInt32(config["Age_Limit"]);
-            string userAgent = config["User_Agent"];
-            string apiAddress = config["Api_Url"];
-            string FeeType = config["Fee_Type"];
-
             DateTime startTime = DateTime.Now;
+            _logger.LogInformation("Worker started at: {time}", DateTimeOffset.Now);
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-
                 HttpClient client = new HttpClient();
                 client.DefaultRequestHeaders.Add("User-Agent", userAgent);
 
-                string dateString = DateTime.Now.ToString("dd-MM-yyyy");
-                string finalURL = string.Format(apiAddress, districtId, dateString);
-
-                HttpResponseMessage response = await client.GetAsync(finalURL);
+                HttpResponseMessage response = await client.GetAsync(this.apiAddress);
                 string responseBody = await response.Content.ReadAsStringAsync();
 
                 var res = JsonConvert.DeserializeObject(responseBody);
@@ -84,7 +90,6 @@ namespace CoWinSlotNotifier
                         return false;
 
                     }).ToList().Any() == true;
-
                 });
 
                 availableCentersNewCall = availableCentersNewCall.Where(center =>
@@ -96,7 +101,7 @@ namespace CoWinSlotNotifier
                 {
                     new ToastContentBuilder()
                     .AddText("There are open slot(s)!")
-                    .AddText("Please open the console window to find out where you can get vaccinated!")
+                    .AddText("Open the console window to see where you can be vaccinated?")
                     .Show();
 
                     availableCentersNewCall.ToList().ForEach(center =>
@@ -109,32 +114,46 @@ namespace CoWinSlotNotifier
                     _logger.LogInformation($"{DateTime.Now.ToString()}: No new open slots found");
                 }
 
+                // Remove centers that don't have vaccines anymore
+                //availableCenters.Keys.ToList().ForEach(key => {
+                //    if (!availableCentersNewCall.Any(c => c.center_id == key)){
+                //        availableCenters.Remove(key);
+                //    }
+                //});
+
                 if (availableCenters.Any())
                 {
                     _logger.LogInformation("Open slots available:");
                     availableCenters.Values.ToList().ForEach(center =>
                     {
-                        string LogLine = $"Pincode: {center.pincode}, Center: {center.name}, Vaccine: {center.sessions[0].vaccine}";
-                        if (age_limit == 0)
-                        {
-                            HashSet<string> ages = new HashSet<string>();
-                            center.sessions.ToList().ForEach(s => ages.Add(s.min_age_limit.ToString()));
-                            LogLine += $" Ages: {String.Join(", ", ages.ToList())}";
-                        }
-                        
+                        string LogLine = $"Pincode: {center.pincode} | Center: {center.name} | Vaccine: {center.sessions[0].vaccine} ";
+
+                        HashSet<string> ages = new HashSet<string>();
+                        HashSet<string> dates = new HashSet<string>();
+                        center.sessions.ToList().ForEach(s => {
+                            if (s.available_capacity > 0)
+                            {
+                                ages.Add(s.min_age_limit.ToString());
+                                dates.Add(s.date.ToString() + " - " + s.available_capacity);
+                            }
+                        });
+
+                        LogLine += $"| Ages: {String.Join(", ", ages.ToList())} ";
+                        LogLine += $"| Dates: {String.Join(", ", dates.ToList())} ";
+
                         Console.WriteLine(LogLine);
                     });
                 }
 
-                Console.WriteLine("-------------------------------------------------------------------------------------------------");
-
-                if (DateTime.Now > startTime.AddHours(6))
+                if (DateTime.Now > startTime.AddMinutes(1))
                 {
                     startTime = DateTime.Now;
                     availableCenters.Clear();
                 }
 
-                await Task.Delay(60000, stoppingToken);
+                Console.WriteLine("-------------------------------------------------------------------------------------------------");
+
+                await Task.Delay(10000, stoppingToken);
             }
         }
     }
